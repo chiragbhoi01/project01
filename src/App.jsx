690471d0c6de46fde0cb322ecaa1d3c63b9b3b6c
chiragbhoi01 <kumarchirag8890@gmail.com>
2025-03-07 08:20:34 +0530

    Add image slider component with react-slick and new assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import brandLogo from "../src/assets/image/Miss-Gypsy-Logo.png";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 import './index.css'
 
 function App() {
   return (
     <>
     <Header/>
+    <Hero/>
     <Footer/>
     </>
   );
