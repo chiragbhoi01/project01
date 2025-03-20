9bc06565a06fca1f73910b558a43d67d04213aea
chiragbhoi01 <kumarchirag8890@gmail.com>
2025-03-21 00:18:37 +0530

    Enhance Vite configuration by adding server sourcemap option and setting up path alias for src directory

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    sourcemap: false
+    sourcemap: false, // Disable sourcemaps for production build
+  },
+  server: {
+    sourcemap: false, // Optionally disable source maps for dev as well
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
   }
 });
